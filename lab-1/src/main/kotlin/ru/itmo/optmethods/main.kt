44a40c4a768504f3c97b75dce4c6cc761cfaf11c
stanislav.feponov <stanislav.feponov@corp.mail.ru>
2021-04-02 21:05:35 +0300

    add gradient convergence chart

--- lab-1/src/main/kotlin/ru/itmo/optmethods/main.kt
+++ lab-1/src/main/kotlin/ru/itmo/optmethods/main.kt
@@ -1,8 +1,10 @@
 package ru.itmo.optmethods
 
+import ru.itmo.optmethods.methods.gradient.comparison.GradientStepsFinderComparison
 import ru.itmo.optmethods.methods.onedim.comparison.OneDimMethodsComparison
 
 
 fun main() {
     OneDimMethodsComparison.compare()
+    GradientStepsFinderComparison.compare()
 }
--- lab-1/src/main/kotlin/ru/itmo/optmethods/methods/gradient/comparison/GradientStepsFinderComparison.kt
+++ lab-1/src/main/kotlin/ru/itmo/optmethods/methods/gradient/comparison/GradientStepsFinderComparison.kt
@@ -1,6 +1,5 @@
 package ru.itmo.optmethods.methods.gradient.comparison
 
-import ru.itmo.optmethods.common.saveToCSV
 import ru.itmo.optmethods.functions.Gradient
 import ru.itmo.optmethods.functions.TwoDimFunction
 import ru.itmo.optmethods.methods.MinimizationMethod
@@ -8,44 +7,74 @@ import ru.itmo.optmethods.methods.MinimizationResult
 import ru.itmo.optmethods.methods.gradient.GradientMethod
 import ru.itmo.optmethods.methods.onedim.DichotomyMethod
 import ru.itmo.optmethods.methods.onedim.GoldenRatioMethod
+import ru.itmo.optmethods.plot.plot
+import ru.itmo.optmethods.plot.points
 
-// x^2 + 2y^2 - xy + x + y + 3
-val func = TwoDimFunction { x, y -> x * x + 2 * y * y - x * y + x + y + 3 }
-val grad = Gradient { (x, y) ->
-    listOf(
-        2 * x - y + 1,
-        4 * y - x + 1
-    )
-}
+object GradientStepsFinderComparison {
 
-fun main() {
-    val grad = GradientMethod(1e-3)
-    writeStats("Dichotomy", runGradientWith(DichotomyMethod(), grad))
-    writeStats("Golder_Ration", runGradientWith(GoldenRatioMethod(), grad))
-    writeStats("Constant", runGradientWith(GradientMethod.ConstantStep(0.25), grad))
-    // TODO add another
-    //val dichRes = runGradientWith(DichotomyMethod(), grad)
-}
+    // x^2 + 2y^2 - xy + x + y + 3
+    val func = TwoDimFunction { x, y -> x * x + 2 * y * y - x * y + x + y + 3 }
+    val grad = Gradient { (x, y) ->
+        listOf(
+            2 * x - y + 1,
+            4 * y - x + 1
+        )
+    }
 
-private fun runGradientWith(
-    stepFinder: MinimizationMethod,
-    gradMethod: GradientMethod
-): List<MinimizationResult> {
-    val results = ArrayList<MinimizationResult>()
-    gradMethod.findMinimum(
-        n = 2,
-        start = listOf(10000.0, 10000.0),
-        function = func,
-        gradient = grad,
-        stepFinder = stepFinder,
-        onStep = { minimizationResult -> results.add(minimizationResult) }
-    )
+    fun compare() {
+        val grad = GradientMethod(1e-3)
+        buildPlots(
+            runGradientWith(DichotomyMethod(), grad),
+            runGradientWith(GoldenRatioMethod(), grad),
+            runGradientWith(GradientMethod.ConstantStep(0.25), grad)
+        )
+    }
 
-    return results
-}
+    private fun runGradientWith(
+        stepFinder: MinimizationMethod,
+        gradMethod: GradientMethod
+    ): List<MinimizationResult> {
+        val results = ArrayList<MinimizationResult>()
+        gradMethod.findMinimum(
+            n = 2,
+            start = listOf(10.0, 10.0),
+            function = func,
+            gradient = grad,
+            stepFinder = stepFinder,
+            onStep = { minimizationResult -> results.add(minimizationResult) }
+        )
 
-private fun writeStats(name: String, res: List<MinimizationResult>) {
-    res.joinToString("\n") { "${it.result}, ${it.iterations}" }.let {
-        saveToCSV(name, it)
+        return results
+    }
+
+    private fun buildPlots(
+        dichotomyResults: List<MinimizationResult>,
+        goldenRatioResults: List<MinimizationResult>,
+        constant: List<MinimizationResult>
+    ) {
+        plot(saveFigPath = "results/gradient/convergence.png") {
+            ylim(2, 20)
+            title("convergence")
+            xlabel("steps")
+            ylabel("value")
+
+            points {
+                label("Golden ratio")
+                add((1..goldenRatioResults.size).toList(), goldenRatioResults.map { it.result })
+            }
+
+            points {
+                linestyle("--")
+                label("Dichotomy")
+                add((1..dichotomyResults.size).toList(), dichotomyResults.map { it.result })
+            }
+
+            points {
+                label("Constant step = 0.25")
+                add((1..constant.size).toList(), constant.map { it.result })
+            }
+
+
+        }
     }
 }
