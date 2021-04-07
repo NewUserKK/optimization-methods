863b2842b88cd42cb2e6f922b0d0e4c137fa6ce6
stanislav.feponov <stanislav.feponov@corp.mail.ru>
2021-04-07 16:51:20 +0300

    work on plots

--- lab-1/src/main/kotlin/ru/itmo/optmethods/plot/PlotUtils.kt
+++ lab-1/src/main/kotlin/ru/itmo/optmethods/plot/PlotUtils.kt
@@ -4,6 +4,8 @@ import com.github.sh0nk.matplotlib4j.NumpyUtils
 import ru.itmo.optmethods.common.Rational
 import ru.itmo.optmethods.functions.TwoDimFunction
 import ru.itmo.optmethods.methods.MinimizationResult
+import java.lang.Double.max
+import java.lang.Double.min
 
 import kotlin.collections.plus
 
@@ -18,24 +20,35 @@ object PlotUtils {
         xMin: Rational, xMax: Rational,
         yMin: Rational, yMax: Rational
     ) {
+        val minX = results.minOf { it.minOf { it.argument[0] } }
+        val maxX = results.maxOf { it.maxOf { it.argument[0] } }
+        val minY = results.minOf { it.minOf { it.argument[1] } }
+        val maxY = results.maxOf { it.maxOf { it.argument[1] } }
+        val lastX = results[0].last().argument[0]
+        val lastY = results[0].last().argument[1]
+        val trueMinX = max(lastX - 10.0, minX)
+        val trueMaxX = min(lastX + 10.0, maxX)
+        val trueMinY = max(lastY - 10.0, minY)
+        val trueMaxY = min(lastY + 10.0, maxY)
+
         plot(saveFigPath = "$path/$title.png") {
             title(title)
 
             val contourBuilder = contour().apply {
-                val steps = 100
-                val xs = NumpyUtils.linspace(results[0].minOf { it.argument[0] }, results[0].maxOf { it.argument[0] }, steps)
-                val ys = NumpyUtils.linspace(results[0].minOf { it.argument[1] }, results[0].maxOf { it.argument[1] }, steps)
+                val steps = 50
+                val xs = NumpyUtils.linspace(trueMinX, trueMaxX, steps)
+                val ys = NumpyUtils.linspace(trueMinY, trueMaxY, steps)
                 val grid = NumpyUtils.meshgrid(xs, ys)
                 val zs = grid.calcZ { xi: Double, yj: Double ->
                     func.invoke(xi, yj)
                 }
                 val levels =
                     NumpyUtils.linspace(
-                        results[0].minOf { it.result },
-                        results[0].maxOf { it.result },
-                        levelsCount
+                        func.invoke(trueMinX, trueMinY),
+                        func.invoke(trueMaxX, trueMaxY),
+                        5
                     )
-                val additional = results[0].map { it.result }.sorted().take(levelsCount)
+                val additional = results[0].map { it.result }.takeLast(40)
                 levels((levels + additional).sorted().distinct())
                 add(xs, ys, zs)
             }
@@ -48,8 +61,8 @@ object PlotUtils {
                     }
                     label(labels[i])
                     add(
-                        res.map { it.argument[0] },
-                        res.map { it.argument[1] }
+                        res.map { it.argument[0].coerceIn(trueMinX, trueMaxX) },
+                        res.map { it.argument[1].coerceIn(trueMinY, trueMaxY) }
                     )
                 }
             }
