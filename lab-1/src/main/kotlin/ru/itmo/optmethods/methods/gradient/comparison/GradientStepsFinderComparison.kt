bb953cbdd939c6bfca5b188c15112af92ca71723
stanislav.feponov <stanislav.feponov@corp.mail.ru>
2021-04-07 23:03:17 +0300

    add stats to gradient comparison

--- lab-1/src/main/kotlin/ru/itmo/optmethods/methods/gradient/comparison/GradientStepsFinderComparison.kt
+++ lab-1/src/main/kotlin/ru/itmo/optmethods/methods/gradient/comparison/GradientStepsFinderComparison.kt
@@ -1,5 +1,6 @@
 package ru.itmo.optmethods.methods.gradient.comparison
 
+import ru.itmo.optmethods.common.Rational
 import ru.itmo.optmethods.comparison.MethodComparison
 import ru.itmo.optmethods.functions.Gradient
 import ru.itmo.optmethods.functions.TwoDimFunction
@@ -7,9 +8,12 @@ import ru.itmo.optmethods.methods.MinimizationMethod
 import ru.itmo.optmethods.methods.MinimizationResult
 import ru.itmo.optmethods.methods.gradient.GradientMethod
 import ru.itmo.optmethods.methods.onedim.DichotomyMethod
+import ru.itmo.optmethods.methods.onedim.FibonacciMethod
 import ru.itmo.optmethods.methods.onedim.GoldenRatioMethod
+import ru.itmo.optmethods.methods.onedim.LinearSearchMethod
 import ru.itmo.optmethods.plot.plot
 import ru.itmo.optmethods.plot.points
+import kotlin.math.abs
 
 object GradientStepsFinderComparison : MethodComparison {
     // x^2 + 2y^2 - xy + x + y + 3
@@ -22,12 +26,46 @@ object GradientStepsFinderComparison : MethodComparison {
     }
 
     override fun compare() {
-        val grad = GradientMethod(1e-3)
-        buildPlots(
-            runGradientWith(DichotomyMethod(), grad),
-            runGradientWith(GoldenRatioMethod(), grad),
-            runGradientWith(GradientMethod.ConstantStep(0.25), grad)
+        val grad = GradientMethod()
+        buildConvergencePlot(
+            "results/gradient",
+            "convergence",
+            listOf("Dichotomy", "Golden Ratio", "Fibonacci", "Constant Step = 0.25", "linear"),
+            listOf(
+                runGradientWith(DichotomyMethod(), grad),
+                runGradientWith(GoldenRatioMethod(), grad),
+                runGradientWith(FibonacciMethod(), grad),
+                runGradientWith(GradientMethod.ConstantStep(0.25), grad),
+                runGradientWith(LinearSearchMethod(), grad)
+            ),
+            17.0 / 7
         )
+    }
+
+    private fun buildConvergencePlot(
+        path: String,
+        fileName: String,
+        titles: List<String>,
+        results: List<List<MinimizationResult>>,
+        realRes: Rational
+    ) {
+        plot(saveFigPath = "$path/$fileName.png") {
+            ylim(-3.0, 3.0)
+            title("convergence")
+            xlabel("steps")
+            ylabel("diff")
+
+            results.forEachIndexed { i, it ->
+                points {
+                    when (i) {
+                        3 -> linestyle("--")
+                        4 -> linestyle("-.")
+                    }
+                    label(titles[i])
+                    add((1..it.size).toList(), it.map { abs(realRes - it.result) })
+                }
+            }
+        }
     }
 
     private fun runGradientWith(
@@ -45,36 +83,5 @@ object GradientStepsFinderComparison : MethodComparison {
         )
 
         return results
-    }
-
-    private fun buildPlots(
-        dichotomyResults: List<MinimizationResult>,
-        goldenRatioResults: List<MinimizationResult>,
-        constant: List<MinimizationResult>
-    ) {
-        plot(saveFigPath = "results/gradient/convergence.png") {
-            ylim(2, 20)
-            title("convergence")
-            xlabel("steps")
-            ylabel("value")
-
-            points {
-                label("Golden ratio")
-                add((1..goldenRatioResults.size).toList(), goldenRatioResults.map { it.result })
-            }
-
-            points {
-                linestyle("--")
-                label("Dichotomy")
-                add((1..dichotomyResults.size).toList(), dichotomyResults.map { it.result })
-            }
-
-            points {
-                label("Constant step = 0.25")
-                add((1..constant.size).toList(), constant.map { it.result })
-            }
-
-
-        }
     }
 }
